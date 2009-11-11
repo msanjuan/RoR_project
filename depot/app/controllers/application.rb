98aa9f9e2c941386940e6f2a33da9c19c10f567e
Mercedes San Juan <msanjuan@whitepages.com>
2009-11-10 16:37:59 -0800

    omg yay exclamtion

--- depot/app/controllers/application.rb
+++ depot/app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'de0f9b60f47c074c4eff437668cd954e'
+  #protect_from_forgery # :secret => 'de0f9b60f47c074c4eff437668cd954e'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
